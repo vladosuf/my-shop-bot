74de11fd5c8d5c2ba374b5378502526125e03710
vladosuf <fofanovv73@gmail.com>
2026-08-30 20:47:35 +0700

    Добавлены print для отладки базы данных

--- database.py
+++ database.py
@@ -5,7 +5,8 @@ DB_PATH = "users.db"
 
 def init_db():
     """Создаёт базу данных и таблицу пользователей"""
-    try:
+     print("🔧 ВХОД В init_db()!") 
+     try:
         conn = sqlite3.connect(DB_PATH)
         cursor = conn.cursor()
         cursor.execute("""
